08122a18ea3a3ab9f0071c88bf973b2a4a9bbfea
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2025-03-17 13:55:15 +0100

    bug: update event type

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,38 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import { ReorderGroup, ReorderItem } from 'motion-v'
+
+interface Item {
+  value: string
+}
+
+const initialItems: Item[] = [{value: '🍅 Tomato'}, {value: '🥒 Cucumber'}, {value: '🧀 Cheese'}, {value: '🥬 Lettuce'}]
+const items = ref(initialItems)
+
+function setItems(newItems: Item[]) {
+  items.value = newItems
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <ReorderGroup
+    :values="items"
+    axis="y"
+    class="relative w-[300px]"
+    @update:values="setItems"
+  >
+    <ReorderItem
+      v-for="item in items"
+      :key="item.value"
+      :value="item"
+      drag
+      class="rounded-lg select-none list-none mb-2 cursor-grab w-full py-4 px-6 bg-purple-500 justify-between flex flex-shrink-0"
+    >
+      {{ item }}
+    </ReorderItem>
+  </ReorderGroup>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
+
